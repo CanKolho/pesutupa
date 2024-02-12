d535785af7f17ff249e610b8f2c4204be10d9457
Can Kolho <can.kolho@aalto.fi>
2024-02-12 15:14:37 +0200

    firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_5WMRvOFz92TvkONcXdfsYnmFykgJSgM",
+  authDomain: "pesutupa-sovellus.firebaseapp.com",
+  projectId: "pesutupa-sovellus",
+  storageBucket: "pesutupa-sovellus.appspot.com",
+  messagingSenderId: "489635063793",
+  appId: "1:489635063793:web:50bd1f8ee575f19ef3c650",
+  measurementId: "G-FCJRPVQVEM"
+};
+
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
